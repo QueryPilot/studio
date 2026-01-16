50b570a73d6cc54a7f58dd119fb9d15ed8aaf35f
Hieu Vu <vdhieu.it@gmail.com>
2026-01-16 08:54:40 +0700

    fix(datagrid): use wildcard export for SqlDataSource as specified

--- src/components/DataGrid/sources/index.ts
+++ src/components/DataGrid/sources/index.ts
@@ -1,3 +1,2 @@
 export * from './types';
-export { SqlDataSource as SqlDataSourceImpl } from './SqlDataSource';
-export type { SqlDataSourceConfig } from './SqlDataSource';
+export * from './SqlDataSource';
